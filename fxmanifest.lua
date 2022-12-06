4d476b5e0e02767f5b4f586d97191fbed3e97dea
CuzzeeTV <118926277+Cuzzman@users.noreply.github.com>
2022-12-06 22:35:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'CuzzeeTV#4169'
-description 'QB ATM Robbery'
-version '1.0'
+description 'Serrulata-ATMRobbery'
+version '1.0.0'
 
 shared_script 'config.lua'
 server_script 'server/*.lua'
-client_scripts 'client/*.lua'
+client_scripts {
+    'client/*.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/CircleZone.lua',
+}
 
 
 lua54 'yes'
+
